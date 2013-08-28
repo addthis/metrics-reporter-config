4bb85bd6653e6ae961d07a14952011b62d53c992
cburroughs <christopher@addthis.com>
2013-08-28 17:54:59 -0400

    allow the console output to be redirected somwhere other than stdout

--- src/main/java/com/addthis/metrics/reporter/config/ConsoleReporterConfig.java
+++ src/main/java/com/addthis/metrics/reporter/config/ConsoleReporterConfig.java
@@ -17,6 +17,8 @@ package com.addthis.metrics.reporter.config;
 import com.yammer.metrics.Metrics;
 import com.yammer.metrics.reporting.ConsoleReporter;
 
+import java.io.PrintStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,16 +27,39 @@ public class ConsoleReporterConfig extends AbstractReporterConfig
 {
     private static final Logger log = LoggerFactory.getLogger(ConsoleReporterConfig.class);
 
+    private String outfile = null;
+
+    public String getOutfile()
+    {
+        return outfile;
+    }
+
+    public void setOutfile(String outfile)
+    {
+        this.outfile = outfile;
+    }
+
     @Override
     public boolean enable()
     {
         try
         {
+            PrintStream stream = null;
+            if (outfile != null)
+            {
+                log.info("console reporting will be redirected to {} instead of stdout", outfile);
+                stream = new PrintStream(outfile);
+            }
+            else
+            {
+                stream = System.out;
+            }
+
             // static enable() methods omit the option of specifying a
             // predicate.  Calling constructor and starting manually
             // instead
             final ConsoleReporter reporter = new ConsoleReporter(Metrics.defaultRegistry(),
-                                                                 System.out,
+                                                                 stream,
                                                                  getMetricPredicate());
             reporter.start(getPeriod(), getRealTimeunit());
 
