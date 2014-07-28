611885c94ebd1f81bd0a4b89d4b7b7626589fcf1
cburroughs <christopher@addthis.com>
2014-07-28 10:38:28 -0400

    remove unused import

--- src/main/java/com/addthis/metrics/reporter/config/RiemannReporterConfig.java
+++ src/main/java/com/addthis/metrics/reporter/config/RiemannReporterConfig.java
@@ -18,7 +18,6 @@ import com.yammer.metrics.Metrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RiemannReporterConfig extends AbstractHostPortReporterConfig
