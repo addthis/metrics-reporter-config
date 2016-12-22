b81eaac8db53f3fcacad44fa798a1a2670c02d05
Josh Horwitz <horwitz@addthis.com>
2016-12-22 12:50:47 -0500

    Add checking measurement predicates in addition to patterns

--- reporter-config3/src/main/java/com/addthis/metrics3/reporter/config/MetricFilterTransformer.java
+++ reporter-config3/src/main/java/com/addthis/metrics3/reporter/config/MetricFilterTransformer.java
@@ -15,63 +15,72 @@
 package com.addthis.metrics3.reporter.config;
 
 import com.addthis.metrics.reporter.config.PredicateConfig;
+
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
 import com.codahale.metrics.Metric;
 import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.Timer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MetricFilterTransformer
-{
+import static com.addthis.metrics.reporter.config.PredicateConfig.allowMeasurement;
+
+public class MetricFilterTransformer {
 
     private static final Logger log = LoggerFactory.getLogger(MetricFilterTransformer.class);
 
-    private static class PredicateConfigFilter implements MetricFilter
-    {
+    private static class PredicateConfigFilter implements MetricFilter {
         final PredicateConfig predicate;
 
-        PredicateConfigFilter(PredicateConfig predicate)
-        {
+        PredicateConfigFilter(PredicateConfig predicate) {
             this.predicate = predicate;
         }
 
         @Override
-        public boolean matches(String name, Metric metric)
-        {
-            log.trace("Checking Metric name: {} {}", new Object[] {name, unqualifyMetricName(name)});
-            if (predicate.getUseQualifiedName())
-            {
-                return predicate.allowString(name);
+        public boolean matches(String name, Metric metric) {
+            log.trace("Checking Metric name: {} {}", new Object[]{name, unqualifyMetricName(name)});
+
+            boolean allowString;
+            if (predicate.getUseQualifiedName()) {
+                allowString = predicate.allowString(name);
+            } else {
+                allowString = predicate.allowString(unqualifyMetricName(name));
             }
-            else
-            {
-                return predicate.allowString(unqualifyMetricName(name));
+
+            if (!allowString) {
+                return false;
             }
+
+            String measurement = unqualifyMetricName(name);
+
+            if ((predicate.getMeter() != null) && (metric instanceof Meter)) {
+                return allowMeasurement(name, measurement, predicate.getMeter(), predicate.getMeterPatterns());
+            } else if ((predicate.getHistogram() != null) && (metric instanceof Histogram)) {
+                return allowMeasurement(name, measurement, predicate.getHistogram(), predicate.getHistogramPatterns());
+            } else if ((predicate.getTimer() != null) && (metric instanceof Timer)) {
+                return allowMeasurement(name, measurement, predicate.getTimer(), predicate.getTimerPatterns());
+            }
+
+            return true;
         }
 
-        private static String unqualifyMetricName(String name)
-        {
+        private static String unqualifyMetricName(String name) {
             int location = name.lastIndexOf('.');
-            if (location < 0)
-            {
+            if (location < 0) {
                 return name;
-            }
-            else
-            {
+            } else {
                 return name.substring(location + 1);
             }
         }
 
     }
 
-    public static MetricFilter generateFilter(PredicateConfig predicate)
-    {
-        if (predicate == null)
-        {
+    public static MetricFilter generateFilter(PredicateConfig predicate) {
+        if (predicate == null) {
             return MetricFilter.ALL;
-        }
-        else
-        {
+        } else {
             return new PredicateConfigFilter(predicate);
         }
     }
